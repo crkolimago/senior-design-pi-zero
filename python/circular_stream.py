a2a534e24660a51f8e86de798dd6541e6409e97f
pi <crkolimago@gmail.com>
2020-02-08 04:21:56 +0000

    python files for demo

--- python/circular_stream.py
+++ python/circular_stream.py
@@ -1,17 +1,30 @@
+import io
 import picamera
+from gpiozero import Button
 
-with picamera.PiCamera() as camera:
-    with picamera.PiCameraCircularIO(camera, seconds=10) as stream:
-        camera.start_recording(stream, format='h264')
-        try:
-            while True:
-                camera.wait_recording(1)
-                # if detect_crash():
-                    # print('Crash detected!')
-                    # camera.split_recording('after.h264')
-                    # Write the 10 seconds before motion to disk
-                    # stream.copy_to('before.h264', seconds=10)
-                    # stream.clear()
-                    # camera.split_recording(stream)
-        finally:
+TIME = 5
+
+button = Button(14)
+
+camera = picamera.PiCamera()
+stream = picamera.PiCameraCircularIO(camera, seconds=TIME) # e.g. at t=TIME+1 t=0 will be deleted
+camera.start_recording(stream, format='h264')
+
+seconds = 0
+
+try:
+    while True:
+        print('recording for %d' % seconds)
+        seconds += 1
+        camera.wait_recording(1)
+        if button.is_pressed:
+            print('button pressed recording for %d more seconds' % TIME)
+            camera.wait_recording(TIME)
+            stream.copy_to('motion.h264')
             camera.stop_recording()
+            print('recording stopped')
+            camera.close()
+finally:
+    camera.stop_recording()
+    print('recording stopped')
+    camera.close()
