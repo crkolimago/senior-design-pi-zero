2a7346ed8265aadeae9ae776af8f2da1b548ec74
Ryan Rohm <rjr61@pitt.edu>
2020-02-12 15:17:16 -0500

    g unit

--- accel.py
+++ accel.py
@@ -3,6 +3,7 @@ import time
 import busio
 import adafruit_lsm9ds1
 import math
+
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
@@ -20,6 +21,7 @@ while True:
     accel = get_accel(accel_x,accel_y,accel_z)
     
     print('Acceleration (m/s^2): ({0:0.3f})'.format(accel))
+    print('Force normalized (g): ({0:0.3f})'.format(accel/9.8))
     #print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
     #print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
     #print('Temperature: {0:0.3f}C'.format(temp))
