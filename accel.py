7660c7d7b87b0dcaf901b46724581ad1279e644f
Ryan Rohm <rjr61@pitt.edu>
2020-02-12 16:20:57 -0500

    legs test

--- accel.py
+++ accel.py
@@ -12,7 +12,7 @@ while True:
     #gyro_x, gyro_y, gyro_z = sensor.gyro
    #temp = sensor.temperature
     # Print values.
-    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
+    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z), file = "results.txt")
     #print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
     #print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
     #print('Temperature: {0:0.3f}C'.format(temp))
