cf777e84a56987b76ccf1b9cd3e0945f4520de40
Ryan Rohm <rjr61@pitt.edu>
2020-02-12 16:22:49 -0500

    legs test

--- accel.py
+++ accel.py
@@ -7,12 +7,13 @@ sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
 while True:
     # Read acceleration, magnetometer, gyroscope, temperature.
+    f = open("results.txt", "w")
     accel_x, accel_y, accel_z = sensor.acceleration
     #mag_x, mag_y, mag_z = sensor.magnetic
     #gyro_x, gyro_y, gyro_z = sensor.gyro
    #temp = sensor.temperature
     # Print values.
-    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z), file = "results.txt")
+    print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z), file = f)
     #print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
     #print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
     #print('Temperature: {0:0.3f}C'.format(temp))
