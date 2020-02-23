f89c0d0cb2b096b882888be722cabee3038674df
Matt Gabrin <mag313@pitt.edu>
2020-02-23 22:01:04 +0000

    pushing results

--- accel.py
+++ accel.py
@@ -2,8 +2,6 @@ import board
 import time
 import busio
 import adafruit_lsm9ds1
-import matplotlib.pyplot as plt 
-import pandas as pd
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
 
@@ -12,42 +10,40 @@ try:
     data_y = {}
     data_z = {}
 
-
+    print("acquiring data")
     start_time = time.time()
     while True:
         # Read acceleration, magnetometer, gyroscope, temperature.
-        #f = open("results.txt", "a")
+        f = open("results.txt", "a")
         accel_x, accel_y, accel_z = sensor.acceleration
         accel_x =  0.10197162129779 * accel_x
         accel_y =  0.10197162129779 * accel_y
         accel_z =  0.10197162129779 * accel_z
 
         curr_time = time.time()-start_time
-        data_x[curr_time] = accel_x
+        """data_x[curr_time] = accel_x
         data_y[curr_time] = accel_y
-        data_z[curr_time] = accel_z
+        data_z[curr_time] = accel_z"""
         
         #mag_x, mag_y, mag_z = sensor.magnetic
         #gyro_x, gyro_y, gyro_z = sensor.gyro
     #temp = sensor.temperature
         # Print values.
-        print("acquiring data")
-       # print('Acceleration (m/s^2): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(accel_x, accel_y, accel_z))
+        
+        print('{0:0.3f},{1:0.3f},{2:0.3f},{3:0.3f}'.format(curr_time,accel_x, accel_y, accel_z),file = f)
         #print('Magnetometer (gauss): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(mag_x, mag_y, mag_z))
         #print('Gyroscope (degrees/sec): ({0:0.3f},{1:0.3f},{2:0.3f})'.format(gyro_x, gyro_y, gyro_z))
         #print('Temperature: {0:0.3f}C'.format(temp))
         # Delay for a second.
         time.sleep(.1)
 except:
-    dfx = pd.DataFrame(data = data_x, columns=["Time","accel_x"])
+    """dfx = pd.DataFrame(data = data_x, columns=["Time","accel_x"])
     dfy = pd.DataFrame(data = data_y, columns=["Time","accel_y"])
     dfz = pd.DataFrame(data = data_z, columns=["Time","accel_z"])
     
     plt.plot(data = dfx)
-    plt.plot(data = dfy)
-    plt.plot(data = dfz)
-    print("here")
-    plt.show(block = True)
-    print("finished")
-
-    #f.close()
+    plt.show(block = True)"""
+    #plt.plot(data = dfy)
+    #plt.plot(data = dfz)
+    print("cleaning up")
+    f.close()
