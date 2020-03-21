8e1761366f90c17bf571ffec9169563687b77490
pi <crkolimago@gmail.com>
2020-03-21 21:47:22 +0000

    python recording updated

--- python/circular_stream.py
+++ python/circular_stream.py
@@ -1,30 +1,30 @@
 import io
 import picamera
-from gpiozero import Button
 
-TIME = 5
-
-button = Button(14)
+TOTAL_TIME = 30
+AFTER_EVENT = 10
 
 camera = picamera.PiCamera()
-stream = picamera.PiCameraCircularIO(camera, seconds=TIME) # e.g. at t=TIME+1 t=0 will be deleted
+stream = picamera.PiCameraCircularIO(camera, seconds=TOTAL_TIME) # e.g. at t=TOTAL_TIME+1 t=0 will be deleted
 camera.start_recording(stream, format='h264')
 
 seconds = 0
 
+# stores TOTAL_TIME seconds in memory, when event is detected overwrites 
+# the first AFTER_EVENT seconds so the result is [TOTAL_TIME-AFTER_EVENT, AFTER_EVENT]
+
 try:
     while True:
         print('recording for %d' % seconds)
-        seconds += 1
         camera.wait_recording(1)
-        if button.is_pressed:
-            print('button pressed recording for %d more seconds' % TIME)
-            camera.wait_recording(TIME)
+        if seconds == 40:
+            print('event detected at %d seconds' % seconds)
+            camera.wait_recording(AFTER_EVENT)
+            seconds += 10
             stream.copy_to('motion.h264')
-            camera.stop_recording()
-            print('recording stopped')
-            camera.close()
+            break
+        seconds+=1
 finally:
     camera.stop_recording()
-    print('recording stopped')
+    print('recording stopped at %d seconds' % seconds)
     camera.close()
