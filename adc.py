182e6e081cce22d21d0c80b78d26fb939816bcf6
Ryan Rohm <rjr61@pitt.edu>
2020-03-30 13:31:19 -0400

    wtf

--- adc.py
+++ adc.py
@@ -4,14 +4,11 @@ import spidev
 import time
 
 #Define Variables
-delay = 1
+delay = 0.5
 pad_channel = 0
 
 #Create SPI
 spi = spidev.SpiDev()
-spi2 = spidev.SpiDev()
-spi2.open(1,0)
-spi2.max_speed_hz = 1000000
 spi.open(0, 0)
 spi.max_speed_hz=1000000
 
@@ -21,26 +18,14 @@ def readadc(adcnum):
         return -1
     r = spi.xfer2([1, 8 + adcnum << 4, 0])
     data = ((r[1] & 3) << 8) + r[2]
-    return 
-
-def readadc2(adcnum):
-    # read SPI data from the MCP3008, 8 channels in total
-    if adcnum > 7 or adcnum < 0:
-        return -1
-    r = spi2.xfer2([1, 8 + adcnum << 4, 0])
-    data = ((r[1] & 3) << 8) + r[2]
     return data
 
 try:
     while True:
         pad_value = readadc(pad_channel)
-        pad_value2 = readadc2(pad_channel)
-
         if pad_value > 700: 
-            print("SPI 1 :"+ pad_value)
-        if pad_value2 > 700: 
-            print("SPI 2 " + pad_value2)
-
+            print("---------------------------------------")
+            print("Pressure Pad Value: %d" % pad_value)
         time.sleep(delay)
 except KeyboardInterrupt:
     pass
