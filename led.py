d73e795a7822ba7839ec4625f7ce1bbacf6ab5dd
Ryan Rohm <rjr61@pitt.edu>
2020-03-29 21:01:50 -0400

    a

--- led.py
+++ led.py
@@ -2,7 +2,7 @@ import board
 import neopixel
 pixels = neopixel.NeoPixel(board.D18, 30)
 
-for i in range(len(pixels)):
+for i in range(len(pixels)*2):
     if i%2 == 0: 
         pixels[i]= (255,0,0)
     else: 
